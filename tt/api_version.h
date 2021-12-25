2e1d30d977baedcae86f537fa424f7f0c5a9e2be
starGeek382 <stargeek382@gmail.com>
2021-12-24 23:48:20 -0600

    Updated version number, which I forgot to do.

--- tt/api_version.h
+++ tt/api_version.h
@@ -15,12 +15,12 @@ namespace tt {
 
 
 	// A C-string detailing the Tirous Toolbox library's version.
-	constexpr const tt_char* const api_version_cstr = "version 1.1";
+	constexpr const tt_char* const api_version_cstr = "version 1.2";
 
 	// A value detailing the major level version number of the Tirous Toolbox library.
 	constexpr tt_size api_version_major = 1;
 
 	// A value detailing the major level version number of the Tirous Toolbox library.
-	constexpr tt_size api_version_minor = 1;
+	constexpr tt_size api_version_minor = 2;
 }
 
